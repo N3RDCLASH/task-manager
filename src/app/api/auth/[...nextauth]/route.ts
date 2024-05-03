f98779da0f492bb078e98a5b26088302c6671293
Joel Naarendorp <35632308+N3RDCLASH@users.noreply.github.com>
2024-05-03 08:27:48 -0300

    feat(auth): added google provider and firebase adapter creds to next auth config

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,23 @@
 import NextAuth from "next-auth";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 import { Adapter } from "next-auth/adapters";
+import { cert } from "firebase-admin/app";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
-  providers: [],
-  adapter: FirestoreAdapter() as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY,
+    }),
+  }) as Adapter,
 });
 
 export { handler as GET, handler as POST };
